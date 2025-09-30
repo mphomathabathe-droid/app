47f7538c5d63c913c3a3477fb0f21bc8f3880583
mphomathabathe-droid <mpho4mikayla1@gmail.com>
2025-09-30 19:28:15 +0200

    updated html and js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function refreshWeather (response){
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = response.data.temperature.current;
+    
+}
+
+
+function searchCity(city){
+let apiKey = "b2a5adcct04b33178913oc335f405433";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 
